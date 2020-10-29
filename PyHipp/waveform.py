8b32bdba9fe8e8f371aa1827debc06e9d61c9476
AWS ParallelCluster user <ec2-user@ip-10-0-11-170.ap-southeast-1.compute.internal>
2020-10-29 10:09:35 +0000

    updated

--- PyHipp/waveform.py
+++ PyHipp/waveform.py
@@ -2,6 +2,7 @@ import DataProcessingTools as DPT
 import matplotlib.pyplot as plt
 import hickle as hkl
 import os
+import numpy as np
 
 class Waveform(DPT.DPObject):
     # Please change the class name according to your needs
@@ -55,7 +56,7 @@ class Waveform(DPT.DPObject):
         
         # check on the mountainsort template data and create a DPT object accordingly
         # Example:
-        if <self.data>:
+        if self.data:
             # create object if data is not empty
             DPT.DPObject.create(self, *args, **kwargs)
         else:
